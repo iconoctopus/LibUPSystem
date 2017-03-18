29d2105f489fabbab02e917af4af1e20a6851b01
ykonoclast <ykonoclast@constitution>
2017-03-18 14:33:40 +0100

    Modification des entêtes de license pour passage en GPL, auteur identifié en ykonoclast

--- src/org/duckdns/spacedock/upengine/libupsystem/CoupleJauge.java
+++ src/org/duckdns/spacedock/upengine/libupsystem/CoupleJauge.java
@@ -1,3 +1,19 @@
+/* 
+ * Copyright (C) 2017 ykonoclast
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.duckdns.spacedock.upengine.libupsystem;
 
 import org.duckdns.spacedock.commonutils.ErrorHandler;
@@ -7,7 +23,7 @@ import org.duckdns.spacedock.commonutils.PropertiesHandler;
  * représente l'un des couples de jauges vitales d'un personnage : santé/init ou
  * fatigue/force d'âme. Abrite les mécanismes d'encaisement des dégâts.
  *
- * @author iconoctopus
+ * @author ykonoclast
  */
 class CoupleJauge
 {
